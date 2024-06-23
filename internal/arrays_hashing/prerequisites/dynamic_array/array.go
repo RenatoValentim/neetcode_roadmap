18bb718aa659c0ee1b1f4fb4e6418bf8be7f0154
RenatoValentim <rvrento28@gmail.com>
2024-06-22 23:22:39 -0300

    feat(dynamic_array): add new value for array

--- internal/arrays_hashing/prerequisites/dynamic_array/array.go
+++ internal/arrays_hashing/prerequisites/dynamic_array/array.go
@@ -1,13 +1,26 @@
 package dynamicarray
 
-type array struct {
+type ArrayInput interface {
+	int | int64 | float64 | string
+}
+
+type array[T ArrayInput] struct {
+	items  []T
 	length int
 }
 
-func New() *array {
-	return &array{length: 10}
+func New[T ArrayInput]() *array[T] {
+	return &array[T]{length: 10}
 }
 
-func (a *array) Length() int {
+func (a *array[T]) Length() int {
 	return a.length
+}
+
+func (a *array[T]) Add(item T) {
+	a.items = append(a.items, item)
+}
+
+func (a *array[T]) Items() []T {
+	return a.items
 }
--- internal/arrays_hashing/prerequisites/dynamic_array/array_test.go
+++ internal/arrays_hashing/prerequisites/dynamic_array/array_test.go
@@ -11,8 +11,16 @@ func TestDynamicArray(t *testing.T) {
 	assert := assert.New(t)
 
 	t.Run(`should create an Array with default lenght`, func(t *testing.T) {
-		da := dynamicarray.New()
+		da := dynamicarray.New[int]()
 		assert.NotNil(da)
 		assert.Equal(da.Length(), 10)
+	})
+
+	t.Run(`should add a new value to the array`, func(t *testing.T) {
+		da := dynamicarray.New[string]()
+		da.Add("hello")
+		assert.NotNil(da)
+		assert.Equal(da.Length(), 10)
+		assert.Equal(da.Items(), []string{"hello"})
 	})
 }
